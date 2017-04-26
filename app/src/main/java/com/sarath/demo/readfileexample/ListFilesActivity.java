e08e185bdc073a259cac8e52d1272fe5532c236e
sarath nambiar <snambiar@wallpostdev.com>
2017-04-26 17:04:54 +0530

    initial commit github

---
+++ app/src/main/java/com/sarath/demo/readfileexample/ListFilesActivity.java
@@ -0,0 +1,97 @@
+package com.sarath.demo.readfileexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.thebrownarrow.permissionhelper.ActivityManagePermission;
+import com.thebrownarrow.permissionhelper.PermissionResult;
+import com.thebrownarrow.permissionhelper.PermissionUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListFilesActivity extends ActivityManagePermission {
+
+    List<File> files;
+    ListView listFiles;
+    ArrayList SavedFiles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_files);
+
+        listFiles = (ListView) findViewById(R.id.list);
+        SavedFiles = new ArrayList();
+
+        askPermission();
+
+        listFiles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent mainIntent = new Intent(ListFilesActivity.this, MainActivity.class);
+                mainIntent.putExtra("filePath", SavedFiles.get(position).toString());
+                startActivity(mainIntent);
+            }
+        });
+
+    }
+
+    private void askPermission() {
+        askCompactPermission(PermissionUtils.Manifest_READ_EXTERNAL_STORAGE, new PermissionResult() {
+            @Override
+            public void permissionGranted() {
+                //permission granted
+                //replace with your action
+
+                files = getListFiles(new File("/sdcard"));
+                Log.e("list", " " + files);
+
+                for (int j = 0; j < files.size(); j++) {
+                    SavedFiles.add(files.get(j).toString());
+                }
+
+                ArrayAdapter<String> adapter
+                        = new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1,
+                        SavedFiles);
+
+                listFiles.setAdapter(adapter);
+            }
+
+            @Override
+            public void permissionDenied() {
+                //permission denied
+                //replace with your action
+            }
+
+            @Override
+            public void permissionForeverDenied() {
+                // user has check never ask again
+                // you need to open setting manually
+                openSettingsApp(ListFilesActivity.this);
+            }
+        });
+    }
+
+    private List<File> getListFiles(File parentDir) {
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files = parentDir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                inFiles.addAll(getListFiles(file));
+            } else {
+                if (file.getName().endsWith(".txt")) {
+                    inFiles.add(file);
+                }
+            }
+        }
+        return inFiles;
+    }
+}
---
+++ app/src/main/java/com/sarath/demo/readfileexample/MainActivity.java
@@ -0,0 +1,120 @@
+package com.sarath.demo.readfileexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import com.thebrownarrow.permissionhelper.ActivityManagePermission;
+import com.thebrownarrow.permissionhelper.PermissionResult;
+import com.thebrownarrow.permissionhelper.PermissionUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MainActivity extends ActivityManagePermission {
+
+    File sdcard;
+    File file;
+    String filepath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        filepath = getIntent().getStringExtra("filePath").toString();
+        Log.e("filepath",""+ filepath);
+
+        initialize();
+
+        askPermission();
+
+
+
+
+    }
+
+    private void askPermission() {
+        askCompactPermission(PermissionUtils.Manifest_READ_EXTERNAL_STORAGE, new PermissionResult() {
+            @Override
+            public void permissionGranted() {
+                //permission granted
+                //replace with your action
+                readFile();
+            }
+
+            @Override
+            public void permissionDenied() {
+                //permission denied
+                //replace with your action
+            }
+
+            @Override
+            public void permissionForeverDenied() {
+                // user has check never ask again
+                // you need to open setting manually
+                openSettingsApp(MainActivity.this);
+            }
+        });
+    }
+
+    private void initialize() {
+        sdcard = Environment.getExternalStorageDirectory();
+        //Get the text file
+        file = new File(filepath);
+    }
+
+    private void readFile() {
+
+
+        //Read text from file
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        } catch (IOException e) {
+            //You'll need to add proper error handling here
+        }
+
+        Log.e("text-->", "" + text);
+
+        try {
+            countWords();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void countWords() throws IOException {
+        Map<String, Integer> countByWords = new HashMap<>();
+        Scanner s = new Scanner(new File(filepath));
+        while (s.hasNext()) {
+            String next = s.next();
+            Integer count = countByWords.get(next);
+            if (count != null) {
+                countByWords.put(next, count + 1);
+            } else {
+                countByWords.put(next, 1);
+            }
+        }
+
+        Log.e("tag", countByWords.toString());
+
+        s.close();
+    }
+
+
+}
