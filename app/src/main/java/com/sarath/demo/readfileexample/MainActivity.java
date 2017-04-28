6ed7dd3cbd91682045861d40c95d5dba6ed8b17b
sarath nambiar <snambiar@wallpostdev.com>
2017-04-28 12:34:17 +0530

    final commit

--- app/src/main/java/com/sarath/demo/readfileexample/MainActivity.java
+++ app/src/main/java/com/sarath/demo/readfileexample/MainActivity.java
@@ -1,103 +1,88 @@
 package com.sarath.demo.readfileexample;
 
-import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
-import android.widget.ExpandableListView;
-
-import com.thebrownarrow.permissionhelper.ActivityManagePermission;
-import com.thebrownarrow.permissionhelper.PermissionResult;
-import com.thebrownarrow.permissionhelper.PermissionUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
-public class MainActivity extends ActivityManagePermission {
+public class MainActivity extends ListActivity {
 
     File sdcard;
     File file;
     String filepath;
+    Map<String, Integer> countByWords;
+    int header_count = 1000, initial_val = 0;
+    private CustomAdapter mAdapter;
 
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         filepath = getIntent().getStringExtra("filePath").toString();
-        Log.e("filepath",""+ filepath);
-
         initialize();
-
-        askPermission();
-
-        // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.lvExp);
-
-        // preparing list data
+        readFile();
         prepareListData();
-
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
-
     }
 
     private void prepareListData() {
 
-        listDataHeader.add("0-10");
-        listDataHeader.add("10-20");
-
-    }
-
-    private void askPermission() {
-        askCompactPermission(PermissionUtils.Manifest_READ_EXTERNAL_STORAGE, new PermissionResult() {
-            @Override
-            public void permissionGranted() {
-                //permission granted
-                //replace with your action
-                readFile();
-            }
-
-            @Override
-            public void permissionDenied() {
-                //permission denied
-                //replace with your action
-            }
-
-            @Override
-            public void permissionForeverDenied() {
-                // user has check never ask again
-                // you need to open setting manually
-                openSettingsApp(MainActivity.this);
+        Set<Map.Entry<String, Integer>> set = countByWords.entrySet();
+        List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, java.lang.Integer>>(set);
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
             }
         });
+
+        mAdapter = new CustomAdapter(this);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (header_count > list.get(i).getValue()) {
+                int result = roundUp(list.get(i).getValue());
+
+                int lower_limit = roundDown(list.get(i).getValue());
+
+                if (initial_val != result) {
+                    initial_val = result;
+                    mAdapter.addSectionHeaderItem(lower_limit + "-" + result);
+                }
+
+                header_count = list.get(i).getValue();
+            }
+            mAdapter.addItem(list.get(i).getKey() + " ( No of occurance : " + list.get(i).getValue() + " ) ");
+        }
+
+        setListAdapter(mAdapter);
+    }
+
+    int roundUp(int n) {
+        return (n + 9) / 10 * 10;
+    }
+
+    int roundDown(int n) {
+        return (n - 9) / 10 * 10;
     }
 
     private void initialize() {
         sdcard = Environment.getExternalStorageDirectory();
-        //Get the text file
         file = new File(filepath);
     }
 
     private void readFile() {
-
-
-        //Read text from file
         StringBuilder text = new StringBuilder();
-
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
@@ -108,11 +93,7 @@ public class MainActivity extends ActivityManagePermission {
             }
             br.close();
         } catch (IOException e) {
-            //You'll need to add proper error handling here
         }
-
-        Log.e("text-->", "" + text);
-
         try {
             countWords();
         } catch (IOException e) {
@@ -122,7 +103,7 @@ public class MainActivity extends ActivityManagePermission {
     }
 
     private void countWords() throws IOException {
-        Map<String, Integer> countByWords = new HashMap<>();
+        countByWords = new HashMap<>();
         Scanner s = new Scanner(new File(filepath));
         while (s.hasNext()) {
             String next = s.next();
@@ -133,12 +114,6 @@ public class MainActivity extends ActivityManagePermission {
                 countByWords.put(next, 1);
             }
         }
-
-        Log.e("tag", countByWords.toString());
-
-
         s.close();
     }
-
-
 }
